68569b031bbf2a10f01cda17fd390c3889af068d
Sergio Rivera Rios <sergioriverarios@Sergios-MacBook-Pro.local>
2026-07-24 17:39:31 -0600

    script for atlantis server

---
+++ atlantis_server.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "enter ngrok server"
+read URL_NGROK
+
+WEBHOOK_URL="${URL_NGROK}/events
+SECRET="I95NYL7l318vBNttJEpT"
+TOKEN="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"
+USERNAME="4387827"
+REPO_ALLOWLIST=ar
+clear:wq
+github.com/SergioRiveraRios/3TierTF.git"
+
+
