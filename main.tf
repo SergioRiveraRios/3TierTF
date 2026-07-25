b0b24c508b63e5171a87c25a92599a5981c9a8a9
Sergio Rivera Rios <sergioriverarios@Sergios-MacBook-Pro.local>
2026-07-24 19:52:25 -0600

    restored

--- main.tf
+++ main.tf
@@ -34,14 +34,6 @@ module "ec2" {
   source = "./modules/ec2"
   subnet-id = module.network.aws-appsubnet-groupAZ2
   security-group = module.security.aws-app-securitygroup-app
-  #comments
-  #more comment
-  #sdfsdfsdf
-  #more 
-  #more
-  #more
-  #adding more line
-  #more
   }
 module "s3" {
   source = "./modules/s3"
