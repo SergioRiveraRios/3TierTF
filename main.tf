e3bbb43ad063d4c7a9f0ce9a45fc1b7043e2f4a4
Sergio Rivera Rios <sergioriverarios@Sergios-MacBook-Pro.local>
2026-08-04 17:03:56 -0600

    test

--- main.tf
+++ main.tf
@@ -8,9 +8,6 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
-  access_key = ""
-  secret_key = ""
 }
 
 module "network" {
@@ -34,12 +31,24 @@ module "ec2" {
   source = "./modules/ec2"
   subnet-id = module.network.aws-appsubnet-groupAZ2
   security-group = module.security.aws-app-securitygroup-app
-  #test 100000
+  subnet-id2 = module.network.aws-websubnet-group1
+  security-group2 = module.security.aws-web-securitygroup-app
+  subnet-id3 = module.network.aws-dbsubnet-groupAZ1
+  security-group3 = module.security.aws-app-securitygroup-app
   }
+
 module "s3" {
   source = "./modules/s3"
   bucket_name = "test123-123"
   bucket_region = "us-east-1"
 }
-
+module "alb" {
+  source = "./modules/ELB"
+  aws-vpc = module.network.vpc-id
+  ALB_security_group = module.security.aws-loadbalancer-external
+  ALB_Subnet1 = module.network.aws-websubnet-group1
+  ALB_Subnet2 = module.network.aws-websubnet-group2
+  ami_webtier = module.ec2.ami_webtier
+  web-tier-security-group = module.security.aws-web-securitygroup-app
+}
 
