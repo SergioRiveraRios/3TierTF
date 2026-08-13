f121bb6b4c375a529166d3af8ccbcd6357d94f0f
Sergio Rivera Rios <sergioriverarios@Sergios-MacBook-Pro.local>
2026-08-13 14:02:38 -0600

    test again

--- modules/ELB/main.tf
+++ modules/ELB/main.tf
@@ -13,7 +13,8 @@ resource "aws_alb" "WebTierALB" {
   subnets            = ["${var.ALB_Subnet1}","${var.ALB_Subnet2}"]
   enable_deletion_protection = true
 }
-#test test
+#test 
+#test2
 resource "aws_lb_listener" "WebTierTG" {
   load_balancer_arn = aws_alb.WebTierALB.arn
   port = "80"
