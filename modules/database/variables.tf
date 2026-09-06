e966ab0cf5c549fedd4775e1ff376aebbfc7320a
Sergio Rivera Rios <sergioriverarios@Sergios-MacBook-Pro.local>
2026-09-06 14:13:34 -0600

    still testing pipeline

--- modules/database/variables.tf
+++ modules/database/variables.tf
@@ -1,13 +1,13 @@
 
 variable "aws-dbsubnetgroupAZ1" {
-    type = string
-    description = "subnets of the db subgroup"
+  type        = string
+  description = "subnets of the db subgroup"
 }
 variable "aws-dbsubnetgroupAZ2" {
-    type = string
-    description = "subnets of the db subgroup"
+  type        = string
+  description = "subnets of the db subgroup"
 }
 variable "aws-db-security-group" {
-  type = string
-    description = "subnets of the db subgroup"
+  type        = string
+  description = "subnets of the db subgroup"
 }
